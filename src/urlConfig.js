39d70a2bfd7ab292f0d664ecb79dd245d0dbd535
sajib679 <shajib.uet@gmail.com>
2021-08-05 03:37:48 +0600

    category image update

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,9 +1,9 @@
-// export const api = "https://ecom-mern-rest-api.herokuapp.com/api";
-// export const imageUrl = (params) => {
-//   return `${api}/upload/${params}`;
-// };
-
-export const api = "http://localhost:2000/api";
+export const api = "https://ecom-mern-rest-api.herokuapp.com/api";
 export const imageUrl = (params) => {
   return `${api}/upload/${params}`;
 };
+
+// export const api = "http://localhost:2000/api";
+// export const imageUrl = (params) => {
+//   return `${api}/upload/${params}`;
+// };
